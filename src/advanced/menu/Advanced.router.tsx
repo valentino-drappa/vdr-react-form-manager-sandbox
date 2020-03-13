4991d8ab917cddda69dc8d5f1da1e287c063e387
vdrappa <valentino.drappa@contractor.voo.be>
2020-03-13 16:06:03 +0100

    change advanced router

--- src/advanced/menu/Advanced.router.tsx
+++ src/advanced/menu/Advanced.router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigation } from '../../commons/component/Navigation.component';
 import { MenuInfos } from '../../commons/component/MenuInfos.components';
 import { RenderOnlyOnChange } from '../renderOnlyOnChange/RenderOnlyOnChange.component';
+import { Redirect } from 'react-router-dom';
 // { path: '/advanced/rendertest', navLabel: 'RenderTest', component: AdvancedTestContainer }
 const navLinks = [{ path: '/advanced/rendertest', navLabel: 'Rendering optimization', component: RenderOnlyOnChange }];
 
@@ -13,6 +14,7 @@ export function AdvancedRouter() {
 				menuDescription="Advanced examples showing how you can use the library for your projects"
 			/>
 			<Navigation navLinks={navLinks} />
+			<Redirect path="advanced" to="/advanced/rendertest" />
 		</React.Fragment>
 	);
 }
